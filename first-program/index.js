9cec9a94970c78eadcaabc3057bd3f156ca5b192
Joy Das <dasjoy365d@gmail.com>
2024-07-30 20:22:33 +0600

    first program added

--- first-program/index.js
+++ first-program/index.js
@@ -1,2 +1,11 @@
 var userName = "Joy Das";
+var age = 23;
+var gender = "Male";
 console.log(userName);
+console.log(age);
+console.log(gender);
+function addNumbers(num1, num2) {
+    var sum = num1 + num2;
+    return sum;
+}
+console.log(addNumbers(10, 5));
--- first-program/index.ts
+++ first-program/index.ts
@@ -1,2 +1,14 @@
-let userName: string = "Joy Das";
+let userName:string = "Joy Das";
+let age:number = 23;
+let gender:string = "Male";
+
 console.log(userName);
+console.log(age);
+console.log(gender);
+
+
+function addNumbers (num1:number, num2:number) {
+    let sum = num1 + num2;
+    return sum;
+}
+console.log(addNumbers(10, 5));
