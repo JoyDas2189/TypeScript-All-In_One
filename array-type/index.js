285b41f0100beef672b1f62cbf47ce73ad30af32
Joy Das <dasjoy365d@gmail.com>
2024-07-30 20:57:43 +0600

    array-type added

---
+++ array-type/index.js
@@ -0,0 +1,6 @@
+var userName = ["Joy", "Luffy", "Zoro", "D - teach"];
+// let userName:Array<string> = ['Joy', 'Luffy', 'Zoro'];
+var userAge = [23, 17, 36];
+console.log(userName[0]);
+var multiTypeArray = ["Joy", 23];
+console.log(multiTypeArray);
---
+++ array-type/index.ts
@@ -0,0 +1,8 @@
+let userName: string[] = ["Joy", "Luffy", "Zoro", "D - teach"];
+// let userName:Array<string> = ['Joy', 'Luffy', 'Zoro'];
+let userAge: number[] = [23, 17, 36];
+
+console.log(userName[0]);
+
+let multiTypeArray: (string | number)[] = ["Joy", 23];
+console.log(multiTypeArray);
