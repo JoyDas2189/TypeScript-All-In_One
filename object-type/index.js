94d4b39074026e6a35fc3ed508d44fddfc613206
Joy Das <joy.softifybd@gmail.com>
2024-07-31 10:58:33 +0600

    object type added

---
+++ object-type/index.js
@@ -0,0 +1,17 @@
+var users;
+users = [];
+var user1;
+user1 = { userName: "Joy", userId: 201 };
+users.push(user1);
+var user2;
+user2 = { userName: "Rahim", userId: 301 };
+users.push(user2);
+console.log(users);
+// for (const key in users) {
+//     console.log(users[key]);
+// }
+// let users : object[];
+// users = [{userName: "joy", userId: 201},
+//          {userName: "das", useId:301},
+// ]
+// console.log(users);
---
+++ object-type/index.ts
@@ -0,0 +1,27 @@
+let users : object[];
+users = [];
+
+
+
+let user1 : {userName: string, userId:number};
+user1 = {userName:"Joy", userId: 201};
+users.push(user1);
+
+let user2 : {userName:string, userId:number}
+user2 = {userName:"Rahim", userId: 301};
+users.push(user2);
+
+console.log(users);
+
+// for (const key in users) {
+//     console.log(users[key]);
+// }
+
+
+
+
+// let users : object[];
+// users = [{userName: "joy", userId: 201},
+//          {userName: "das", useId:301},
+// ]
+// console.log(users);
